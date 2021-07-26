c4c4b9da26c589fceebaf2d9604eb665be92a8ec
Lucas Cardoso Tavares <81261317+carvares@users.noreply.github.com>
2021-07-26 14:55:13 -0300

    Update musicSchema.ts

--- src/schema/musicSchema.ts
+++ src/schema/musicSchema.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const schema = Joi.object({
     name: Joi.string().trim().min(1).pattern(/^[0-9A-Za-zÀ-úç'\s]+$/).required(),
-    youtubeLink: Joi.string().min(1).required()
+    youtubeLink: Joi.string().uri().min(1).required()
 });
 
 export {schema};
