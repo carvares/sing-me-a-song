7e638e78d899f1fc4fef3f603dd6e135ea8cc8fc
leleo <celso@respondeai.com.br>
2021-07-27 16:51:16 -0300

    preparando review

--- src/schema/musicSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const schema = Joi.object({
-    name: Joi.string().trim().min(1).pattern(/^[0-9A-Za-zÀ-úç'\s]+$/).required(),
-    youtubeLink: Joi.string().uri().min(1).required()
-});
-
-export {schema};
